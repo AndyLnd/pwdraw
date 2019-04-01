7d1a3919241327080814be87a79a930399c09c1d
AndyLnd <andy@wire.com>
2019-04-01 23:26:36 +0200

    format index, ios status bar style

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 
@@ -10,60 +10,62 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader",
-            options: { minimize: true }
-          }
-        ]
-      }
-    ]
+            loader: 'html-loader',
+            options: { minimize: true },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
+      template: './src/index.html',
+      filename: './index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    }),  
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
     new WebpackPwaManifest({
       name: 'Doodle',
       short_name: 'Doodle',
       description: 'For doodling',
       inject: true,
-      ios: true,
+      ios: {
+        'apple-mobile-web-app-status-bar-style': 'black-translucent',
+      },
       background_color: '#ffffff',
       icons: [
         {
           src: path.resolve('src/icon.png'),
           sizes: [120, 152, 167, 180, 1024],
           destination: path.join('icon'),
-          ios: true
+          ios: true,
         },
         {
           src: path.resolve('src/icon.png'),
           size: 1024,
           destination: path.join('icon'),
-          ios: 'startup'
+          ios: 'startup',
         },
         {
           src: path.resolve('src/icon.png'),
           sizes: [36, 48, 72, 96, 144, 192, 512],
-          destination: path.join('icon')
-        }
-      ]
-    })
+          destination: path.join('icon'),
+        },
+      ],
+    }),
   ],
-  devtool: 'source-map'
+  devtool: 'source-map',
 };
